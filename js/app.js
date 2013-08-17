efa0edc30f2ae8eb73f23a13481b0df6c3dd2c61
Benno Rice <benno@jeamland.net>
2013-08-17 21:46:09 +1000

    Add ticket viewer. Changes state json blob format.

--- js/app.js
+++ js/app.js
@@ -108,3 +108,73 @@ function BallotPickerCtrl($scope, $http, $location, $window) {
     });
 
 }
+
+function TicketViewerCtrl($scope, $http, $location) {
+    var state = $location.path();
+
+    $scope.ticket = [null, null];
+    $scope.candidateOrder = [[], []];
+
+    $scope.ticketList = [];
+
+    $scope.changeTicket = function (idx) {
+        console.log(">>> " + $scope.ticket[idx]);
+
+        if (!$scope.ticket[idx]) {
+            $scope.candidateOrder[idx] = $scope.ballotOrder;
+            return;
+        }
+
+        var data = $scope.ticket[idx].split('-');
+        var group = data[0];
+        var ticket = parseInt(data[1]) - 1;
+
+        $scope.candidateOrder[idx] = $scope.groups[group].tickets[ticket];
+    }
+
+    function generateTicketList() {
+        var tickets = [];
+
+        angular.forEach($scope.groups, function (group, group_id) {
+            if (group.tickets.length == 1) {
+                tickets.push({
+                    id: "" + group_id + "-1",
+                    name: group.name
+                });
+            } else {
+                angular.forEach(group.tickets, function (ticket, idx) {
+                    tickets.push({
+                        id: "" + group_id + "-" + (idx + 1),
+                        name: group.name + " (" + (idx + 1) + " of " + group.tickets.length + ")"
+                    });
+                });
+            }
+        });
+
+        $scope.ticketList = tickets;
+    }
+
+    $http.get('/state' + state + '.json').
+        success(function (data) {
+            console.log(data);
+            $scope.state = data.state;
+            $scope.stateOrTerritory = data.state_or_territory;
+            $scope.candidates = data.candidates;
+            $scope.ballotOrder = data.ballot_order;
+            $scope.groups = data.groups;
+
+            $scope.candidateOrder = [$scope.ballotOrder, $scope.ballotOrder];
+
+            generateTicketList();
+        }).
+        error(function () {
+            console.log("I have no idea");
+        });
+    $http.get('/parties.json').
+        success(function (data) {
+            $scope.parties = data;
+        }).
+        error(function () {
+            console.log("I have no idea");
+        });
+}
