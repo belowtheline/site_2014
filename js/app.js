358a8837184db9b527b965c62133365f3f2d163b
Michael Pearson <mipearson@gmail.com>
2013-09-01 14:41:46 +1000

    Move generateTicketLis to app.js so that the ballotpicker can use it.

--- js/app.js
+++ js/app.js
@@ -22,6 +22,48 @@ function ($q , $rootScope , options){
   }
 }]);
 
+var generateTicketList = function(scopeGroups) {
+
+  var groups = [];
+  var tickets = [];
+
+  angular.forEach(scopeGroups, function (group, group_id) {
+      groups.push(group_id);
+  });
+
+  groups.sort(function (a, b) {
+      if (a.length != b.length) {
+          return a.length - b.length;
+      } else if (a < b) {
+          return -1;
+      } else if (b > a) {
+          return 1;
+      } else {
+          return 0;
+      }
+  });
+
+  angular.forEach(groups, function (group_id, idx) {
+      var group = scopeGroups[group_id];
+
+      if (group.tickets.length == 1) {
+          tickets.push({
+              id: group_id + "1",
+              name: group.name
+          });
+      } else {
+          angular.forEach(group.tickets, function (ticket, idx) {
+              tickets.push({
+                  id: group_id + (idx + 1),
+                  name: group.name + " (" + (idx + 1) + " of " + group.tickets.length + ")"
+              });
+          });
+      }
+  });
+
+  return tickets;
+}
+
 angular.module('belowtheline', ['ui.sortable', 'ngGeolocation']).
     config(['$locationProvider', function ($locationProvider) {
             $locationProvider.html5Mode(true);
